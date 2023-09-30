af01a7fbafe9a881986ed06b71a985efe8bdf167
Fikri Haikal <kontak.haikal@outlook.com>
2023-09-30 18:12:23 +0800

    added copy plugin

--- vite.config.ts
+++ vite.config.ts
@@ -2,11 +2,36 @@ import { fileURLToPath, URL } from 'node:url'
 
 import vue from '@vitejs/plugin-vue'
 import vueJsx from '@vitejs/plugin-vue-jsx'
-import { defineConfig } from 'vite'
+import { copyFileSync } from 'node:fs'
+import { resolve } from 'node:path'
+import { defineConfig, Plugin } from 'vite'
+
+type Target = {
+  src: string
+  dest: string
+}
+
+const copy = (target: Target[]): Plugin => ({
+  name: 'Copy Files',
+  async writeBundle() {
+    target
+      .map(({ src, dest }) => ({ src: resolve(src), dest: resolve(dest) }))
+      .forEach(({ src, dest }: Target) => copyFileSync(src, dest))
+  }
+})
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue(), vueJsx()],
+  plugins: [
+    vue(),
+    vueJsx(),
+    copy([
+      {
+        src: './_redirects',
+        dest: './dist/redirects'
+      }
+    ])
+  ],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
