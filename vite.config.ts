cc5c977add05cf347763888dcddb761dbc515056
Fikri Haikal <kontak.haikal@outlook.com>
2023-09-30 18:19:03 +0800

    add redirect config for netlify

--- vite.config.ts
+++ vite.config.ts
@@ -2,36 +2,11 @@ import { fileURLToPath, URL } from 'node:url'
 
 import vue from '@vitejs/plugin-vue'
 import vueJsx from '@vitejs/plugin-vue-jsx'
-import { copyFileSync } from 'node:fs'
-import { resolve } from 'node:path'
-import { defineConfig, Plugin } from 'vite'
-
-type Target = {
-  src: string
-  dest: string
-}
-
-const copy = (target: Target[]): Plugin => ({
-  name: 'Copy Files',
-  async writeBundle() {
-    target
-      .map(({ src, dest }) => ({ src: resolve(src), dest: resolve(dest) }))
-      .forEach(({ src, dest }: Target) => copyFileSync(src, dest))
-  }
-})
+import { defineConfig } from 'vite'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-    vue(),
-    vueJsx(),
-    copy([
-      {
-        src: './_redirects',
-        dest: './dist/redirects'
-      }
-    ])
-  ],
+  plugins: [vue(), vueJsx()],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
